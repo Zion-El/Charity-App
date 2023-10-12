c29fdaab1ea3227cb34ca31d31d7fcf27920b486
Michael Adelola <mchladelola@gmail.com>
2023-10-12 11:03:44 +0100

    updated

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -5,7 +5,11 @@ const Account = () => {
   <div className='a-wrap'>
     <div className='a-wrap-content'>
       <h2>Donate To Our Cause Today</h2>
-      <div className="hr"></div>
+      <div className="hr">
+        <div></div>
+        <div className="circle"></div>
+        <div></div>
+      </div>
       <p>Account Name:  Naomi Hope Foundation Ltd</p>
       <p>Account Number:  0547272639</p>
       <p>Bank: Sterling Bank Plc</p>
